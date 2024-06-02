cf8600d06190e959dc191f44fb6ffde3b5593a6a
navidik <navidmohammed007@gmail.com>
2024-06-02 08:58:08 +0530

    learning exception handling

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=20;
+        int c=0;
+        try{
+            int b =a/c;
+        }
+        catch (Exception e){
+            System.out.println("Error ");
+        }
+        System.out.println("b : " );
+    }
+}
